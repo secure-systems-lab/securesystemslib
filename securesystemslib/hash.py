a2371f43c481ab24743f159082f31e392291c41f
Vladimir Diaz <vladimir.v.diaz@gmail.com>
2017-09-28 12:04:46 -0400

    Refactor and remove PyCrypto from hash.py

--- securesystemslib/hash.py
+++ securesystemslib/hash.py
@@ -13,14 +13,13 @@
   See LICENSE for licensing information.
 
 <Purpose>
-  Support multiple implementations of secure hash and message digest
-  algorithms. Any hash-related routines that TUF requires should be
-  located in this module.  Ensuring that a secure hash algorithm is
-  available to TUF, simplifying the creation of digest objects, and
-  providing a central location for hash routines are the main goals
-  of this module.  Support routines implemented include functions to
-  create digest objects given a filename or file object.  Hashlib and PyCrypto
-  hash algorithms currently supported.
+  Support secure hashing and message digests. Any hash-related routines that
+  TUF requires should be located in this module.  Simplifying the creation of
+  digest objects, and providing a central location for hash routines are the
+  main goals of this module.  Support routines implemented include functions to
+  create digest objects given a filename or file object.  Only the standard
+  hashlib library is currently supported, but pyca/crytography support will be
+  added in the future.
 """
 
 # Help with Python 3 compatibility, where the print statement is a function, an
@@ -32,75 +31,31 @@ from __future__ import division
 from __future__ import unicode_literals
 
 import logging
+import hashlib
 
 import six
 
 import securesystemslib.exceptions
+import securesystemslib.formats
 
 # Import securesystemslib logger to log warning messages.
 logger = logging.getLogger('securesystemslib.hash')
 
-# The list of hash libraries imported successfully.
-_supported_libraries = []
-
-# Hash libraries currently supported by securesystemslib.hash.
-_SUPPORTED_LIB_LIST = ['hashlib', 'pycrypto']
-
-# Let's try importing the pycrypto hash algorithms.  Pycrypto will
-# not be added to the supported list of libraries if the specified
-# hash algorithms below cannot all be imported.
-try:
-  from Crypto.Hash import MD5
-  from Crypto.Hash import SHA
-  from Crypto.Hash import SHA224
-  from Crypto.Hash import SHA256
-  from Crypto.Hash import SHA384
-  from Crypto.Hash import SHA512
-  _supported_libraries.append('pycrypto')
-
-except ImportError: # pragma: no cover
-  logger.debug('Pycrypto hash algorithms could not be imported.  '
-              'Supported libraries: '+str(_SUPPORTED_LIB_LIST))
-
-  pass
-
-# Python <=2.4 does not have the hashlib module by default.
-# Let's try importing hashlib and adding it to our supported list.
-try:
-  import hashlib
-  _supported_libraries.append('hashlib')
-
-except ImportError: # pragma: no cover
-  logger.debug('Hashlib could not be imported.  '
-              'Supported libraries: '+str(_SUPPORTED_LIB_LIST))
-  pass
-
-# Were we able to import any hash libraries?
-if not _supported_libraries: # pragma: no cover
-  # This is fatal, we'll have no way of generating hashes.
-  raise securesystemslib.exceptions.Error('Unable to import a hash library from the '
-                  'following supported list: '+str(_SUPPORTED_LIB_LIST))
+DEFAULT_HASH_ALGORITHM = 'sha256'
+DEFAULT_HASH_LIBRARY = 'hashlib'
+SUPPORTED_LIBRARIES = ['hashlib']
 
 
-_DEFAULT_HASH_ALGORITHM = 'sha256'
-_DEFAULT_HASH_LIBRARY = 'hashlib'
-
-
-
-
-
-def digest(algorithm=_DEFAULT_HASH_ALGORITHM,
-           hash_library=_DEFAULT_HASH_LIBRARY):
+def digest(algorithm=DEFAULT_HASH_ALGORITHM, hash_library=DEFAULT_HASH_LIBRARY):
   """
   <Purpose>
-    Provide the caller with the ability to create
-    digest objects without having to worry about hash
-    library availability or which library to use.
-    The caller also has the option of specifying which
-    hash algorithm and/or library to use.
+    Provide the caller with the ability to create digest objects without having
+    to worry about crypto library availability or which library to use.  The
+    caller also has the option of specifying which hash algorithm and/or
+    library to use.
 
-    # Creation of a digest object using defaults
-    # or by specifying hash algorithm and library.
+    # Creation of a digest object using defaults or by specifying hash
+    # algorithm and library.
     digest_object = securesystemslib.hash.digest()
     digest_object = securesystemslib.hash.digest('sha384')
     digest_object = securesystemslib.hash.digest('pycrypto')
@@ -117,64 +72,59 @@ def digest(algorithm=_DEFAULT_HASH_ALGORITHM,
 
   <Arguments>
     algorithm:
-      The hash algorithm (e.g., md5, sha1, sha256).
+      The hash algorithm (e.g., 'md5', 'sha1', 'sha256').
 
     hash_library:
-      The library providing the hash algorithms
-      (e.g., pycrypto, hashlib).
+      The crypto library to use for the given hash algorithm (e.g., 'hashlib').
 
   <Exceptions>
-    securesystemslib.exceptions.UnsupportedAlgorithmError
-    securesystemslib.exceptions.UnsupportedLibraryError
+    securesystemslib.exceptions.FormatError, if the arguments are
+    improperly formatted.
+
+    securesystemslib.exceptions.UnsupportedAlgorithmError, if an unsupported
+    hashing algorithm is specified, or digest could not be generated with given
+    the algorithm.
+
+    securesystemslib.exceptions.UnsupportedLibraryError, if an unsupported
+    library was requested via 'hash_library'.
 
   <Side Effects>
     None.
 
   <Returns>
-    Digest object (e.g., hashlib.new(algorithm) or
-    algorithm.new() # pycrypto).
+    Digest object (e.g., hashlib.new(algorithm)).
   """
+
+  # Are the arguments properly formatted?  If not, raise
+  # 'securesystemslib.exceptions.FormatError'.
+  securesystemslib.formats.NAME_SCHEMA.check_match(algorithm)
+  securesystemslib.formats.NAME_SCHEMA.check_match(hash_library)
 
   # Was a hashlib digest object requested and is it supported?
   # If so, return the digest object.
-  if hash_library == 'hashlib' and hash_library in _supported_libraries:
+  if hash_library == 'hashlib' and hash_library in SUPPORTED_LIBRARIES:
     try:
       return hashlib.new(algorithm)
 
     except ValueError:
       raise securesystemslib.exceptions.UnsupportedAlgorithmError(algorithm)
 
-  # Was a pycrypto digest object requested and is it supported?
-  elif hash_library == 'pycrypto' and hash_library in _supported_libraries:
-    # Pycrypto does not offer a comparable hashlib.new(hashname).
-    # Let's first check the 'algorithm' argument before returning
-    # the correct pycrypto digest object using pycrypto's object construction.
-    if algorithm == 'md5':
-      return MD5.new()
-    elif algorithm == 'sha1':
-      return SHA.new()
-    elif algorithm == 'sha224':
-      return SHA224.new()
-    elif algorithm == 'sha256':
-      return SHA256.new()
-    elif algorithm == 'sha384':
-      return SHA384.new()
-    elif algorithm == 'sha512':
-      return SHA512.new()
-    else:
-      raise securesystemslib.exceptions.UnsupportedAlgorithmError(algorithm)
+  # Was a pyca_crypto digest object requested and is it supported?
+  elif hash_library == 'pyca_crypto' and hash_library in SUPPORTED_LIBRARIES:
+    # TODO: Add support for pyca/cryptography's hashing routines.
+    pass
 
   # The requested hash library is not supported.
   else:
-    raise securesystemslib.exceptions.UnsupportedLibraryError('Unsupported library requested.  '
-                    'Supported hash libraries: '+str(_SUPPORTED_LIB_LIST))
+    raise securesystemslib.exceptions.UnsupportedLibraryError('Unsupported'
+        ' library requested.  Supported hash libraries: ' + repr(SUPPORTED_LIBRARIES))
 
 
 
 
 
-def digest_fileobject(file_object, algorithm=_DEFAULT_HASH_ALGORITHM,
-                      hash_library=_DEFAULT_HASH_LIBRARY):
+def digest_fileobject(file_object, algorithm=DEFAULT_HASH_ALGORITHM,
+    hash_library=DEFAULT_HASH_LIBRARY):
   """
   <Purpose>
     Generate a digest object given a file object.  The new digest object
@@ -187,29 +137,37 @@ def digest_fileobject(file_object, algorithm=_DEFAULT_HASH_ALGORITHM,
       to update the hash of a digest object to be returned.
 
     algorithm:
-      The hash algorithm (e.g., md5, sha1, sha256).
+      The hash algorithm (e.g., 'md5', 'sha1', 'sha256').
 
     hash_library:
-      The library providing the hash algorithms
-      (e.g., pycrypto, hashlib).
+      The library providing the hash algorithms (e.g., 'hashlib').
 
   <Exceptions>
-    securesystemslib.exceptions.UnsupportedAlgorithmError
+    securesystemslib.exceptions.FormatError, if the arguments are
+    improperly formatted.
 
-    securesystemslib.exceptions.Error
+    securesystemslib.exceptions.UnsupportedAlgorithmError, if an unsupported
+    hashing algorithm was specified via 'algorithm'.
+
+    securesystemslib.exceptions.UnsupportedLibraryError, if an unsupported
+    crypto library was specified via 'hash_library'.
 
   <Side Effects>
-    Calls securesystemslib.hash.digest() to create the actual digest object.
+    None.
 
   <Returns>
-    Digest object (e.g., hashlib.new(algorithm) or
-    algorithm.new() # pycrypto).
+    Digest object (e.g., hashlib.new(algorithm)).
   """
+
+  # Are the arguments properly formatted?  If not, raise
+  # 'securesystemslib.exceptions.FormatError'.
+  securesystemslib.formats.NAME_SCHEMA.check_match(algorithm)
+  securesystemslib.formats.NAME_SCHEMA.check_match(hash_library)
 
   # Digest object returned whose hash will be updated using 'file_object'.
   # digest() raises:
   # securesystemslib.exceptions.UnsupportedAlgorithmError
-  # securesystemslib.exceptions.Error
+  # securesystemslib.exceptions.UnsupportedLibraryError
   digest_object = digest(algorithm, hash_library)
 
   # Defensively seek to beginning, as there's no case where we don't
@@ -237,8 +195,8 @@ def digest_fileobject(file_object, algorithm=_DEFAULT_HASH_ALGORITHM,
 
 
 
-def digest_filename(filename, algorithm=_DEFAULT_HASH_ALGORITHM,
-                    hash_library=_DEFAULT_HASH_LIBRARY):
+def digest_filename(filename, algorithm=DEFAULT_HASH_ALGORITHM,
+    hash_library=DEFAULT_HASH_LIBRARY):
   """
   <Purpose>
     Generate a digest object, update its hash using a file object
@@ -249,34 +207,41 @@ def digest_filename(filename, algorithm=_DEFAULT_HASH_ALGORITHM,
       The filename belonging to the file object to be used.
 
     algorithm:
-      The hash algorithm (e.g., md5, sha1, sha256).
+      The hash algorithm (e.g., 'md5', 'sha1', 'sha256').
 
     hash_library:
-      The library providing the hash algorithms
-      (e.g., pycrypto, hashlib).
+      The library providing the hash algorithms (e.g., 'hashlib').
 
   <Exceptions>
-    securesystemslib.exceptions.UnsupportedAlgorithmError
-    securesystemslib.exceptions.Error
+    securesystemslib.exceptions.FormatError, if the arguments are
+    improperly formatted.
+
+    securesystemslib.exceptions.UnsupportedAlgorithmError, if the given
+    'algorithm' is unsupported.
+
+    securesystemslib.exceptions.UnsupportedLibraryError, if the given
+    'hash_library' is unsupported.
 
   <Side Effects>
-    Calls securesystemslib.hash.digest_fileobject() after opening 'filename'.
-    File closed before returning.
+    None.
 
   <Returns>
-    Digest object (e.g., hashlib.new(algorithm) or
-    algorithm.new() # pycrypto).
+    Digest object (e.g., hashlib.new(algorithm)).
   """
+  # Are the arguments properly formatted?  If not, raise
+  # 'securesystemslib.exceptions.FormatError'.
+  securesystemslib.formats.RELPATH_SCHEMA.check_match(filename)
+  securesystemslib.formats.NAME_SCHEMA.check_match(algorithm)
+  securesystemslib.formats.NAME_SCHEMA.check_match(hash_library)
+
+  digest_object = None
 
   # Open 'filename' in read+binary mode.
-  file_object = open(filename, 'rb')
-
-  # Create digest_object and update its hash data from file_object.
-  # digest_fileobject() raises:
-  # securesystemslib.exceptions.UnsupportedAlgorithmError
-  # securesystemslib.exceptions.Error
-  digest_object = digest_fileobject(file_object, algorithm, hash_library)
-
-  file_object.close()
+  with open(filename, 'rb') as file_object:
+    # Create digest_object and update its hash data from file_object.
+    # digest_fileobject() raises:
+    # securesystemslib.exceptions.UnsupportedAlgorithmError
+    # securesystemslib.exceptions.UnsupportedLibraryError
+    digest_object = digest_fileobject(file_object, algorithm, hash_library)
 
   return digest_object
