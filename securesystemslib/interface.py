cde07b4132422e985e5612ce72baf64a36df2fdb
Vladimir Diaz <vladimir.v.diaz@gmail.com>
2018-01-11 16:36:23 -0500

    Support KEYID filenames for ECDSA keypairs
    Signed-off-by: Vladimir Diaz <vladimir.v.diaz@gmail.com>

--- securesystemslib/interface.py
+++ securesystemslib/interface.py
@@ -382,7 +382,7 @@ def generate_and_write_ed25519_keypair(filepath=None, password=None):
     The 'filepath' of the written key.
   """
 
-  # Generate a new Ed25519 key object and encrypt it.
+  # Generate a new Ed25519 key object.
   ed25519_key = securesystemslib.keys.generate_ed25519_key()
 
   if not filepath:
@@ -585,21 +585,26 @@ def import_ed25519_privatekey_from_file(filepath, password=None):
 
 
 
-def generate_and_write_ecdsa_keypair(filepath, password=None):
+def generate_and_write_ecdsa_keypair(filepath=None, password=None):
   """
   <Purpose>
-    Generate an ECDSA key file, create an encrypted key (using 'password' as
-    the pass phrase), and store it in 'filepath'.  The public key portion of
-    the generated ECDSA key is stored in <'filepath'>.pub.  The 'cryptography'
-    library currently supported.  The private key is encrypted according to
-    'cryptography's approach: "Encrypt using the best available encryption for
-    a given key's backend. This is a curated encryption choice and the
-    algorithm may change over time."
+    Generate an ECDSA keypair, where the encrypted key (using 'password' as the
+    passphrase) is saved to <'filepath'>.  The public key portion of the
+    generated ECDSA key is saved to <'filepath'>.pub.  If the filepath is not
+    given, the KEYID is used as the filename and the keypair saved to the
+    current working directory.
+
+    The 'cryptography' library currently supported.  The private key is
+    encrypted according to 'cryptography's approach: "Encrypt using the best
+    available encryption for a given key's backend. This is a curated
+    encryption choice and the algorithm may change over time."
 
   <Arguments>
     filepath:
       The public and private key files are saved to <filepath>.pub and
-      <filepath>, respectively.
+      <filepath>, respectively.  If the filepath is not given, the public and
+      private keys are saved to the current working directory as <KEYID>.pub
+      and <KEYID>.  KEYID is the generated key's KEYID.
 
     password:
       The password, or passphrase, to encrypt the private portion of the
@@ -616,8 +621,19 @@ def generate_and_write_ecdsa_keypair(filepath, password=None):
     Writes key files to '<filepath>' and '<filepath>.pub'.
 
   <Returns>
-    None.
+    The 'filepath' of the written key.
   """
+
+  # Generate a new ECDSA key object.  The 'cryptography' library is currently
+  # supported and performs the actual cryptographic operations.
+  ecdsa_key = securesystemslib.keys.generate_ecdsa_key()
+
+  if not filepath:
+    filepath = os.path.join(os.getcwd(), ecdsa_key['keyid'])
+
+  else:
+    logger.debug('The filepath has been specified.  Not using the key\'s'
+        ' KEYID as the default filepath.')
 
   # Does 'filepath' have the correct format?
   # Ensure the arguments have the appropriate number of objects and object
@@ -635,25 +651,11 @@ def generate_and_write_ecdsa_keypair(filepath, password=None):
         ' key (' + Fore.RED + relative_path + Fore.RESET + '): ',
         confirm=False)
 
+  else:
+    logger.debug('The password has been specified.  Not prompting for one')
+
   # Does 'password' have the correct format?
   securesystemslib.formats.PASSWORD_SCHEMA.check_match(password)
-
-  # Generate a new ECDSA key object and encrypt it.  The 'cryptography' library
-  # is currently supported and performs the actual cryptographic routine.
-  # Raise 'securesystemslib.exceptions.CryptoError' if 'ed25519_key' cannot be
-  # encrypted.
-  ecdsa_key = securesystemslib.keys.generate_ecdsa_key()
-  encrypted_key = securesystemslib.keys.encrypt_key(ecdsa_key, password)
-
-  # ECDSA public key file contents in metadata format (i.e., does not include
-  # the keyid portion).
-  keytype = ecdsa_key['keytype']
-  keyval = ecdsa_key['keyval']
-  scheme = ecdsa_key['scheme']
-
-  ecdsakey_metadata_format = \
-    securesystemslib.keys.format_keyval_to_metadata(keytype, scheme,
-        keyval, private=False)
 
   # Write the public key, conformant to 'securesystemslib.formats.KEY_SCHEMA',
   # to '<filepath>.pub'.
@@ -662,6 +664,15 @@ def generate_and_write_ecdsa_keypair(filepath, password=None):
   # Create a tempororary file, write the contents of the public key, and move
   # to final destination.
   file_object = securesystemslib.util.TempFile()
+
+  # Generate the ECDSA public key file contents in metadata format (i.e., does
+  # not include the keyid portion).
+  keytype = ecdsa_key['keytype']
+  keyval = ecdsa_key['keyval']
+  scheme = ecdsa_key['scheme']
+  ecdsakey_metadata_format = securesystemslib.keys.format_keyval_to_metadata(
+      keytype, scheme, keyval, private=False)
+
   file_object.write(json.dumps(ecdsakey_metadata_format).encode('utf-8'))
 
   # The temporary file is closed after the final move.
@@ -670,9 +681,13 @@ def generate_and_write_ecdsa_keypair(filepath, password=None):
   # Write the encrypted key string, conformant to
   # 'securesystemslib.formats.ENCRYPTEDKEY_SCHEMA', to '<filepath>'.
   file_object = securesystemslib.util.TempFile()
+  # Raise 'securesystemslib.exceptions.CryptoError' if 'ecdsa_key' cannot be
+  # encrypted.
+  encrypted_key = securesystemslib.keys.encrypt_key(ecdsa_key, password)
   file_object.write(encrypted_key.encode('utf-8'))
   file_object.move(filepath)
 
+  return filepath
 
 
 
