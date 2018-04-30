d1ba2bb8986e262f42198c32ec656952f12f1c92
Vladimir Diaz <vladimir.v.diaz@gmail.com>
2018-04-30 11:34:21 -0400

    Bump release version in setup.py
    Signed-off-by: Vladimir Diaz <vladimir.v.diaz@gmail.com>

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ with open('README.rst') as file_object:
 
 setup(
   name = 'securesystemslib',
-  version = '0.11.0',
+  version = '0.11.1',
   description = 'A library that provides cryptographic and general-purpose routines for Secure Systems Lab projects at NYU',
   long_description = long_description,
   author = 'https://www.updateframework.com',
