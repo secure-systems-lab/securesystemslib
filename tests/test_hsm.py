e9a5289e31ddee91f0c1d2505c6311198126baa5
Tanishq Jasoria <jasoriatanishq@gmail.com>
2020-03-21 19:33:35 +0530

    Add function to test load library function

--- tests/test_hsm.py
+++ tests/test_hsm.py
@@ -34,7 +34,7 @@ import securesystemslib.formats
 # To initialize SoftHSM for testing purposes!
 if not six.PY2:
   import PyKCS11
-  import securesystemslib.hsm
+  import securesystemslib.hsm as HSM
 # Library to interact with SoftHSM.
 PKCS11LIB = '/usr/local/lib/softhsm/libsofthsm2.so'
 
@@ -75,22 +75,152 @@ class TestHSM(unittest.TestCase):
 
   @classmethod
   def setUpClass(cls):
-    pass
+
+    # To carry out the tests even when the hardware token is not connected,
+    # we would be emulating the hardware token using softHSM 2.0.
+    # To carry out all the tests, SoftHSM needs to be initialized and
+    # RSD, ECDSA key pairs must be generated on the SoftHSM.
+
+    # Since we are using the default path for the SoftHSM creation
+    # and storage, there might be tokens present already which were
+    # create by the user. So, before carrying out the test we must save all
+    # the existing token to a new directory
+
+    tokens_list = os.listdir(TOKENS_PATH)
+
+    # Make a new directory to store the already existing tokens
+    tokens_save_dir = os.path.join(TOKENS_PATH, 'tokens.save')
+
+    # If the directory already exists, do nothing and use the
+    # pre-existing directory to save the tokens
+    try:
+      os.mkdir(tokens_save_dir)
+    except:
+      logger.info("Directory 'tokens.save' already exists!")
+
+    # Move the tokens to the new directory
+    for token in tokens_list:
+      token_dir = os.path.join(TOKENS_PATH,token)
+      shutil.move(token_dir, tokens_save_dir)
+
+    # Initializing the HSM
+    soft_pkcs11 = PyKCS11.PyKCS11Lib()
+    soft_pkcs11.load(PKCS11LIB)
+    available_hsm = soft_pkcs11.getSlotList()
+    soft_pkcs11.initToken(available_hsm.pop(), _SO_PIN, _HSM_LABEL)
+
+    # After initializing the SoftHSM, the slot number changes.
+    soft_pkcs11.load(PKCS11LIB)
+    available_hsm = soft_pkcs11.getSlotList()
+    session = soft_pkcs11.openSession(available_hsm[0],
+        PyKCS11.CKF_SERIAL_SESSION | PyKCS11.CKF_RW_SESSION)
+    # Login as a SO User to initialize the pin.
+    session.login(_SO_PIN, PyKCS11.CKU_SO)
+    session.initPin(_USER_PIN)
+    session.logout()
+    # Login as admin to generate key pairs.
+    session.login(_USER_PIN)
+
+    # Generate RSA Key Pair on the HSM
+    RSA_public_template = [
+        (PyKCS11.CKA_CLASS, PyKCS11.CKO_PUBLIC_KEY),
+        (PyKCS11.CKA_TOKEN, PyKCS11.CK_TRUE),
+        (PyKCS11.CKA_PRIVATE, PyKCS11.CK_FALSE),
+        (PyKCS11.CKA_MODULUS_BITS, RSA_BITS),
+        (PyKCS11.CKA_PUBLIC_EXPONENT, RSA_EXPONENTS),
+        (PyKCS11.CKA_ENCRYPT, PyKCS11.CK_TRUE),
+        (PyKCS11.CKA_VERIFY, PyKCS11.CK_TRUE),
+        (PyKCS11.CKA_VERIFY_RECOVER, PyKCS11.CK_TRUE),
+        (PyKCS11.CKA_WRAP, PyKCS11.CK_TRUE),
+        (PyKCS11.CKA_LABEL, KEY_LABEL),
+        (PyKCS11.CKA_ID, RSA_KEY_ID),]
+    RSA_private_template = [
+        (PyKCS11.CKA_CLASS, PyKCS11.CKO_PRIVATE_KEY),
+        (PyKCS11.CKA_TOKEN, PyKCS11.CK_TRUE),
+        (PyKCS11.CKA_PRIVATE, PyKCS11.CK_TRUE),
+        (PyKCS11.CKA_DECRYPT, PyKCS11.CK_TRUE),
+        (PyKCS11.CKA_SIGN, PyKCS11.CK_TRUE),
+        (PyKCS11.CKA_SIGN_RECOVER, PyKCS11.CK_TRUE),
+        (PyKCS11.CKA_UNWRAP, PyKCS11.CK_TRUE),
+        (PyKCS11.CKA_LABEL, KEY_LABEL),
+        (PyKCS11.CKA_ID, RSA_KEY_ID),]
+    (cls.RSA_public_key, cls.RSA_private_key) = session.generateKeyPair(
+      RSA_public_template, RSA_private_template, PyKCS11.MechanismRSAGENERATEKEYPAIR)
+
+    # Generate ECDSA key pair on the HSM
+    EC_public_template = [
+        (PyKCS11.CKA_CLASS, PyKCS11.CKO_PUBLIC_KEY),
+        (PyKCS11.CKA_PRIVATE, PyKCS11.CK_FALSE),
+        (PyKCS11.CKA_TOKEN, PyKCS11.CK_TRUE),
+        (PyKCS11.CKA_ENCRYPT, PyKCS11.CK_TRUE),
+        (PyKCS11.CKA_VERIFY, PyKCS11.CK_TRUE),
+        (PyKCS11.CKA_WRAP, PyKCS11.CK_TRUE),
+        (PyKCS11.CKA_KEY_TYPE, PyKCS11.CKK_ECDSA),
+        (PyKCS11.CKA_EC_PARAMS, EC_PARAMS),
+        (PyKCS11.CKA_LABEL, KEY_LABEL),
+        (PyKCS11.CKA_ID, EC_KEY_ID),]
+    EC_private_template = [
+        (PyKCS11.CKA_CLASS, PyKCS11.CKO_PRIVATE_KEY),
+        (PyKCS11.CKA_KEY_TYPE, PyKCS11.CKK_ECDSA),
+        (PyKCS11.CKA_TOKEN, PyKCS11.CK_TRUE),
+        (PyKCS11.CKA_SENSITIVE, PyKCS11.CK_TRUE),
+        (PyKCS11.CKA_DECRYPT, PyKCS11.CK_TRUE),
+        (PyKCS11.CKA_SIGN, PyKCS11.CK_TRUE),
+        (PyKCS11.CKA_UNWRAP, PyKCS11.CK_TRUE),
+        (PyKCS11.CKA_LABEL, KEY_LABEL),
+        (PyKCS11.CKA_ID, EC_KEY_ID),]
+    (cls.EC_public_key, cls.EC_private_key) = session.generateKeyPair(
+      EC_public_template, EC_private_template, PyKCS11.MechanismECGENERATEKEYPAIR)
+
+    # Logout and close all sessions
+    session.logout()
+    session.closeSession()
+
 
 
   @classmethod
   def tearDownClass(cls):
-    pass
+
+    # Remove any new tokens which were initialized to perform tests
+    tokens_list = os.listdir(TOKENS_PATH)
+    tokens_list.remove('tokens.save')
+    for token in tokens_list:
+      token_dir = os.path.join(TOKENS_PATH, token)
+      shutil.rmtree(token_dir)
+
+    # Move the saved tokens to their original directory
+    tokens_save_dir = os.path.join(TOKENS_PATH, 'tokens.save')
+    tokens_save_list = os.listdir(tokens_save_dir)
+    for token in tokens_save_list:
+      token_dir = os.path.join(tokens_save_dir, token)
+      shutil.move(token_dir, TOKENS_PATH)
+
+    # Delete the new directory, used to store existing tokens
+    shutil.rmtree(tokens_save_dir)
+
 
 
   @classmethod
   def setUp(self):
-    pass
+
+    HSM.load_pkcs11_library(PKCS11LIB)
 
 
   @classmethod
   def tearDown(self):
     pass
+
+
+
+  def test_load_pkcs11_library(self):
+
+    PKCS11LIB_ERR = "/NO/LIBRARY/HERE""
+
+    self.assertRaises(securesystemslib.exceptions.NotFoundError,
+        HSM.load_pkcs11_library, PKCS11LIB_ERR)
+
+    # Initialize with a correct library
+    HSM.load_pkcs11_library(PKCS11LIB)
 
 
 
