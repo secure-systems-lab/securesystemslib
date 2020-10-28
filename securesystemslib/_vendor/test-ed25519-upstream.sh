639c9ea0e8e81298992808212c09c66de71edc8d
Jussi Kukkonen <jkukkonen@vmware.com>
2020-10-28 11:27:55 +0200

    github: Add workflow to check ed25519 upstream
    securesystemslib includes a vendored copy of
    https://github.com/pyca/ed25519.git . The upstream is not a very lively
    project by any means but we would like to know if something happens
    there (like a bug fix).
    
    * Add a script that checks if upstream master HEAD is unchanged
      (the expected commit is in the script)
    * Add a github workflow that once a day files an issue if the upstream
      master head has changed and if an issue is not open yet

---
+++ securesystemslib/_vendor/test-ed25519-upstream.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Check for new commits in upstream ed25519
+#
+# Return 0 if the upstream ed25519 master branch HEAD matches the
+# commit that our copy was vendored from.
+#
+# This is used in CI workflow to open an issue if new commits are found
+
+set -eu
+
+# This commit matches our securesystemslib/_vendor/ed25519/ content.
+# If upstream changes, we should review the changes, vendor them,
+# and update the hash here
+pyca_ed25519_expected="18d0b5515c8bda6bab0148f1946845bf3027bc4f"
+pyca_ed25519_git_url="https://github.com/pyca/ed25519.git"
+
+pyca_ed25519_master_head=$(git ls-remote "$pyca_ed25519_git_url" master | cut -f1)
+if [ "$pyca_ed25519_master_head" != "$pyca_ed25519_expected" ]; then
+    echo "$pyca_ed25519_git_url master branch has been updated." >&2
+    echo "Expected $pyca_ed25519_expected, found $pyca_ed25519_master_head." >&2
+    exit 1
+fi
+
+echo "No unexpected commits in https://github.com/pyca/ed25519.git"
