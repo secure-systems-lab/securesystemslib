2bc36c9b44b42dd0e632e8931e288b322b1e3ade
Martin Vrachev <mvrachev@vmware.com>
2021-02-16 17:05:19 +0200

    Skip test_signer on Python2.7
    Signed-off-by: Martin Vrachev <mvrachev@vmware.com>

--- tests/test_signer.py
+++ tests/test_signer.py
@@ -2,11 +2,26 @@
 
 """Test cases for "signer.py". """
 
+import sys
+import unittest
+
 import unittest
 import securesystemslib.formats
 import securesystemslib.keys as KEYS
-from securesystemslib.signer import Signature, SSlibSigner
 from securesystemslib.exceptions import FormatError, UnsupportedAlgorithmError
+
+# TODO: Remove case handling when fully dropping support for versions < 3.6
+IS_PY_VERSION_SUPPORTED = sys.version_info >= (3, 6)
+
+# Use setUpModule to tell unittest runner to skip this test module gracefully.
+def setUpModule():
+    if not IS_PY_VERSION_SUPPORTED:
+        raise unittest.SkipTest("requires Python 3.6 or higher")
+
+# Since setUpModule is called after imports we need to import conditionally.
+if IS_PY_VERSION_SUPPORTED:
+    from securesystemslib.signer import Signature, SSlibSigner
+
 
 class TestSSlibSigner(unittest.TestCase):
 
