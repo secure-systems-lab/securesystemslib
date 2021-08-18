a31d966567b171580506939690fa4745faeca720
Joshua Lock <jlock@vmware.com>
2021-08-18 12:00:54 +0100

    Prepare v0.21.0 release
    Bump version in setup.py and populate CHANGELOG.md with summarised changes
    since the last release.
    
    Signed-off-by: Joshua Lock <jlock@vmware.com>

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ with open('README.rst') as file_object:
 
 setup(
   name = 'securesystemslib',
-  version = '0.20.1',
+  version = '0.21.0',
   description = 'A library that provides cryptographic and general-purpose'
       ' routines for Secure Systems Lab projects at NYU',
   license = 'MIT',
