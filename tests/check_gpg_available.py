e5cabc5a553f419d442d37771c8d218854f6bfe7
Zachary Newman <z@znewman.net>
2022-09-30 14:10:16 -0400

    Double-check that GPG tests run where expected.
    The test suite passes even if GPG is not available, because GPG-dependent
    tests are skipped if GPG is not present.
    
    This change asserts the availability of GPG, so CI environments that expect GPG
    will notice if it goes away unexpectedly rather than silently skipping the GPG
    tests.
    
    Signed-off-by: Zachary Newman <z@znewman.net>

---
+++ tests/check_gpg_available.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+"""
+<Program Name>
+  check_gpg_available.py
+
+<Author>
+  Zack Newman <zjn@chainguard.dev>
+
+<Started>
+  September 30, 2022.
+
+<Copyright>
+  See LICENSE for licensing information.
+
+<Purpose>
+  The test suite passes even if GPG is not available, because GPG-dependent
+  tests are skipped if GPG is not present.
+
+  This file asserts the availability of GPG, so CI environments that expect GPG
+  will notice if it goes away unexpectedly rather than silently skipping the GPG
+  tests.
+
+  NOTE: the filename is purposefully check_ rather than test_ so that test
+  discovery doesn't find this unittest and the tests within are only run
+  when explicitly invoked.
+"""
+
+import unittest
+
+import securesystemslib.gpg.constants
+
+
+class TestGpgAvailable(unittest.TestCase):
+  """Test that securesystemslib finds some GPG executable in the environment."""
+
+  def test_gpg_available(self):
+    """Test that GPG is available."""
+    self.assertTrue(securesystemslib.gpg.constants.HAVE_GPG)
+
+if __name__ == "__main__":
+  unittest.main(verbosity=1, buffer=True)
