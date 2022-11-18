d40527e1d6330b39d6e821bd427e493f66dce494
Jussi Kukkonen <jkukkonen@google.com>
2022-11-18 13:40:54 +0200

    signer: refactor, move to a new submodule
    Public API stays the same:
    * module is still securesystemslib.signer
    * but now we can split implementation to internal submodules
    * __init__.py now clearly defines the public API

---
+++ securesystemslib/signer/__init__.py
@@ -0,0 +1,16 @@
+"""
+The Signer API
+"""
+
+from securesystemslib.signer._signer import (
+    KEY_FOR_TYPE_AND_SCHEME,
+    SIGNER_FOR_URI_SCHEME,
+    GPGSignature,
+    GPGSigner,
+    Key,
+    SecretsHandler,
+    Signature,
+    Signer,
+    SSlibKey,
+    SSlibSigner,
+)
