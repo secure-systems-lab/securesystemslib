917a75b3644b3db517ee3c43960389f6d5f37986
Lukas Puehringer <lukas.puehringer@nyu.edu>
2022-11-30 19:50:27 +0100

    signer: add HSMSigner and HSMKey
    Adds basic Signer and Key implementations to generate signatures on
    a hardware security module (HSMSigner) and to export the corresponding
    public key (HSMKey).
    
    Supported keys are ecdsa on SECG curves secp256r1 (NIST P-256) or
    secp384r1 (NIST P-384), which correspond to securesystemslib
    signing schemes "ecdsa-sha2-nistp256" and "ecdsa-sha2-nistp384".
    
    Tests are performed on SoftHSM (virtual hsm).
    
    **Caveat**
    HSMSigner and HSMKey use the token from a passed PyKCS11 session.
    This means that users must identify the correct slot, token and key,
    open a session, optionally log in (for signing), and also log out
    and close the session afterwards.
    
    This is not user-friendly. Ideally, the user only identifies the
    correct slot, token and key out-of-band (e.g. with pkcs11-tool,
    yubico-piv-tool or ykman) and then passes a stable identifier to
    HSMSigner. Maybe labels? Slot id is not stable.
    
    **Other ideas**
    - HSMKey is an SSlibKey with an *import key from HSM* method.
      The method could be moved to different import API (see #466),
      and HSMKey could be removed.
    - HSMSigner could live in a dedicated _hsm_signer.py, this would
      better hide the conditional imports.
    
    Signed-off-by: Lukas Puehringer <lukas.puehringer@nyu.edu>

---
+++ tests/check_hsm_signer.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python
+"""Test HSMSigner
+"""
+import os
+import shutil
+import tempfile
+import unittest
+
+from asn1crypto.keys import (  # pylint: disable=import-error
+    ECDomainParameters,
+    NamedCurve,
+)
+from cryptography.hazmat.primitives.asymmetric.ec import SECP256R1, SECP384R1
+from PyKCS11 import PyKCS11
+
+import securesystemslib.hash
+from securesystemslib.signer import HSMKey, HSMSigner
+
+
+class TestHSM(unittest.TestCase):
+    """Test HSMSigner and HSMKey with SoftHSM
+
+    Requirements:
+    - install SoftHSM2
+    - set environment variable ``PYKCS11LIB`` to SoftHSM library path
+
+    See .github/workflows/hsm.yml for how this can be done on Linux, macOS and Windows.
+    """
+
+    hsm_user_pin = "1234"
+
+    @classmethod
+    def setUpClass(cls):
+        """Initialize SoftHSM token and generate ecdsa test keys"""
+
+        # Configure SoftHSM to create test token in temporary test directory
+        cls.original_cwd = os.getcwd()
+        cls.test_dir = os.path.realpath(tempfile.mkdtemp())
+        os.chdir(cls.test_dir)
+
+        with open("softhsm2.conf", "w", encoding="utf-8") as f:
+            f.write("directories.tokendir = " + os.path.join(cls.test_dir, ""))
+
+        os.environ["SOFTHSM2_CONF"] = os.path.join(
+            cls.test_dir, "softhsm2.conf"
+        )
+
+        # Initialize test token
+        cls.pkcs11 = PyKCS11.PyKCS11Lib()
+        cls.pkcs11.load()
+        hsm_token_label = "Test SoftHSM"
+        hsm_so_pin = "abcd"
+
+        hsm_slot_id = cls.pkcs11.getSlotList(tokenPresent=True)[0]
+        cls.pkcs11.initToken(hsm_slot_id, hsm_so_pin, hsm_token_label)
+
+        session = cls.pkcs11.openSession(hsm_slot_id, PyKCS11.CKF_RW_SESSION)
+        session.login(hsm_so_pin, PyKCS11.CKU_SO)
+        session.initPin(cls.hsm_user_pin)
+        session.logout()
+
+        session.login(cls.hsm_user_pin)
+
+        # Generate test ecdsa key pairs for curves secp256r1 and secp384r1 on test token
+        cls.hsm_keyids = []
+        for keyid, curve in (
+            ((0,), SECP256R1),
+            ((1,), SECP384R1),
+        ):
+
+            params = ECDomainParameters(
+                name="named", value=NamedCurve(curve.name)
+            ).dump()
+
+            public_template = [
+                (PyKCS11.CKA_CLASS, PyKCS11.CKO_PUBLIC_KEY),
+                (PyKCS11.CKA_PRIVATE, PyKCS11.CK_FALSE),
+                (PyKCS11.CKA_TOKEN, PyKCS11.CK_TRUE),
+                (PyKCS11.CKA_ENCRYPT, PyKCS11.CK_FALSE),
+                (PyKCS11.CKA_VERIFY, PyKCS11.CK_TRUE),
+                (PyKCS11.CKA_WRAP, PyKCS11.CK_FALSE),
+                (PyKCS11.CKA_KEY_TYPE, PyKCS11.CKK_ECDSA),
+                (PyKCS11.CKA_EC_PARAMS, params),
+                (PyKCS11.CKA_LABEL, curve.name),
+                (PyKCS11.CKA_ID, keyid),
+            ]
+            private_template = [
+                (PyKCS11.CKA_CLASS, PyKCS11.CKO_PRIVATE_KEY),
+                (PyKCS11.CKA_KEY_TYPE, PyKCS11.CKK_ECDSA),
+                (PyKCS11.CKA_TOKEN, PyKCS11.CK_TRUE),
+                (PyKCS11.CKA_SENSITIVE, PyKCS11.CK_TRUE),
+                (PyKCS11.CKA_DECRYPT, PyKCS11.CK_FALSE),
+                (PyKCS11.CKA_SIGN, PyKCS11.CK_TRUE),
+                (PyKCS11.CKA_UNWRAP, PyKCS11.CK_FALSE),
+                (PyKCS11.CKA_LABEL, curve.name),
+                (PyKCS11.CKA_ID, keyid),
+            ]
+
+            session.generateKeyPair(
+                public_template,
+                private_template,
+                mecha=PyKCS11.MechanismECGENERATEKEYPAIR,
+            )
+
+            cls.hsm_keyids.append(keyid)
+
+        session.logout()
+        session.closeSession()
+
+    @classmethod
+    def tearDownClass(cls):
+        os.chdir(cls.original_cwd)
+        shutil.rmtree(cls.test_dir)
+        del os.environ["SOFTHSM2_CONF"]
+
+    def test_hsm(self):
+        """Test public key export, HSM signing, and verification w/o HSM"""
+
+        def _pre_hash(data, scheme):
+            """Generate hash for scheme (test hack)"""
+            hasher = securesystemslib.hash.digest(algorithm=f"sha{scheme[-3:]}")
+            hasher.update(data)
+            return hasher.digest()
+
+        hsm_slot_id = self.pkcs11.getSlotList(tokenPresent=True)[0]
+        session = self.pkcs11.openSession(hsm_slot_id)
+
+        keyid = (
+            "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+        )
+        data = b"deadbeef"
+
+        for hsm_keyid in self.hsm_keyids:
+            public_key = HSMKey.from_hsm(session, hsm_keyid, keyid)
+
+            session.login(self.hsm_user_pin)  # Login for signing
+            signer = HSMSigner(session, hsm_keyid, public_key)
+
+            # NOTE: HSMSigner supports CKM_ECDSA_SHA256 and CKM_ECDSA_SHA384
+            # mechanisms. But SoftHSM only supports CKM_ECDSA. During testing we
+            # patch the HSMSigner mechanisms and pre-hash the data ourselves.
+            signer._mechanism = (  # pylint: disable=protected-access
+                PyKCS11.Mechanism(PyKCS11.CKM_ECDSA)
+            )
+            sig = signer.sign(_pre_hash(data, public_key.scheme))
+
+            session.logout()  # Logout after signing
+
+            public_key.verify_signature(sig, data)
+
+        session.closeSession()
+
+
+# Run the unit tests.
+if __name__ == "__main__":
+    unittest.main()
