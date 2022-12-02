5a7700f2de1bf54715a3b4bf29ade75f09c014dd
Lukas Puehringer <lukas.puehringer@nyu.edu>
2022-12-02 17:31:49 +0100

    signer: remove HSMKey but keep from_hsm method
    HSMKey is only useful for its from_hsm method. This method is
    temporarily moved to HSM test module, as verbatim copy
    with some error handling commented out, and will likely be
    moved to the HSMSigner in a subsequent commit.
    
    Signed-off-by: Lukas Puehringer <lukas.puehringer@nyu.edu>

--- securesystemslib/signer/__init__.py
+++ securesystemslib/signer/__init__.py
@@ -5,12 +5,7 @@ This module provides extensible interfaces for public keys and signers:
 Some implementations are provided by default but more can be added by users.
 """
 from securesystemslib.signer._gcp_signer import GCPSigner
-from securesystemslib.signer._key import (
-    KEY_FOR_TYPE_AND_SCHEME,
-    HSMKey,
-    Key,
-    SSlibKey,
-)
+from securesystemslib.signer._key import KEY_FOR_TYPE_AND_SCHEME, Key, SSlibKey
 from securesystemslib.signer._signature import GPGSignature, Signature
 from securesystemslib.signer._signer import (
     SIGNER_FOR_URI_SCHEME,
