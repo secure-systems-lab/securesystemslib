60679b70dbb28c007688f98080c96817f7e24c81
Lukas Puehringer <lukas.puehringer@nyu.edu>
2022-12-06 15:54:28 +0100

    signer: add public key export method to HSMSigner
    Move pub key export, previously on the no longer existing
    HSMKey class, and then temporarily a test method, to HSMSigner.
    
    This should not be testing code but public API.
    
    Signed-off-by: Lukas Puehringer <lukas.puehringer@nyu.edu>

--- securesystemslib/signer/_hsm_signer.py
+++ securesystemslib/signer/_hsm_signer.py
@@ -4,6 +4,14 @@
 # pylint: disable=wrong-import-position
 CRYPTO_IMPORT_ERROR = None
 try:
+    from cryptography.hazmat.primitives import serialization
+    from cryptography.hazmat.primitives.asymmetric.ec import (
+        SECP256R1,
+        SECP384R1,
+        EllipticCurvePublicKey,
+        ObjectIdentifier,
+        get_curve_for_oid,
+    )
     from cryptography.hazmat.primitives.asymmetric.utils import (
         encode_dss_signature,
     )
@@ -11,13 +19,22 @@ except ImportError:  # pragma: no cover
     CRYPTO_IMPORT_ERROR = "'cryptography' required"
 
 PYKCS11_IMPORT_ERROR = None
-PYKCSLIB = None
 try:
     from PyKCS11 import PyKCS11
 
 except ImportError:  # pragma: no cover
     PYKCS11_IMPORT_ERROR = "'PyKCS11' required"
+
+ASN1_IMPORT_ERROR = None
+try:
+    from asn1crypto.keys import (  # pylint: disable=import-error
+        ECDomainParameters,
+        ECPoint,
+    )
+except ImportError:
+    ASN1_IMPORT_ERROR = "'asn1crypto' required"
 # pylint: enable=wrong-import-position
+
 
 import binascii
 from typing import Optional
@@ -57,6 +74,10 @@ class HSMSigner(Signer):
     """
 
     SCHEME = "hsm"
+    # For now, we only support keyid 2, i.e. PIV slot 9c (Digital Signature)
+    # https://developers.yubico.com/PIV/Introduction/Certificate_slots.html
+    # https://developers.yubico.com/yubico-piv-tool/YKCS11/
+    PIV_9C_KEYID = 2
 
     def __init__(
         self, hsm_keyid: int, public_key: Key, secrets_handler: SecretsHandler
@@ -81,6 +102,91 @@ class HSMSigner(Signer):
         self.public_key = public_key
         self.secrets_handler = secrets_handler
 
+    @staticmethod
+    def pubkey_from_hsm(hsm_keyid: int, sslib_keyid: str) -> SSlibKey:
+        """Export public key from HSM
+
+        Supports ecdsa on SECG curves secp256r1 (NIST P-256) or secp384r1 (NIST P-384).
+
+        Arguments:
+            hsm_keyid: Key identifier on the token.
+            sslib_keyid: Key identifier that is unique within the metadata it is used in.
+
+        Raises:
+            UnsupportedLibraryError: ``PyKCS11``, ``cryptography`` or ``asn1crypto``
+                    libraries not found.
+            ValueError: No compatible key for ``hsm_keyid`` found on HSM.
+            PyKCS11.PyKCS11Error: Various HSM communication errors.
+
+        """
+        if CRYPTO_IMPORT_ERROR:
+            raise UnsupportedLibraryError(CRYPTO_IMPORT_ERROR)
+
+        if PYKCS11_IMPORT_ERROR:
+            raise UnsupportedLibraryError(PYKCS11_IMPORT_ERROR)
+
+        if ASN1_IMPORT_ERROR:
+            raise UnsupportedLibraryError(ASN1_IMPORT_ERROR)
+
+        lib = PYKCS11LIB()
+        hsm_slot_id = lib.getSlotList(tokenPresent=True)[0]
+        session = lib.openSession(hsm_slot_id)
+
+        # Search for ecdsa public keys with passed keyid on HSM
+        keys = session.findObjects(
+            [
+                (PyKCS11.CKA_CLASS, PyKCS11.CKO_PUBLIC_KEY),
+                (PyKCS11.CKA_KEY_TYPE, PyKCS11.CKK_ECDSA),
+                (PyKCS11.CKA_ID, (hsm_keyid,)),
+            ]
+        )
+
+        if len(keys) != 1:
+            raise ValueError(
+                f"hsm_keyid must identify one {KEY_TYPE_ECDSA} key, found {len(keys)}"
+            )
+
+        # Extract public key domain parameters and point from HSM
+        hsm_params, hsm_point = session.getAttributeValue(
+            keys[0], [PyKCS11.CKA_EC_PARAMS, PyKCS11.CKA_EC_POINT]
+        )
+
+        params = ECDomainParameters.load(bytes(hsm_params))
+
+        session.closeSession()
+
+        # TODO: Define as module level constant and don't hardcode scheme strings
+        scheme_for_curve = {
+            SECP256R1: "ecdsa-sha2-nistp256",
+            SECP384R1: "ecdsa-sha2-nistp384",
+        }
+        curve_names = [curve.name for curve in scheme_for_curve]
+
+        if params.chosen.native not in curve_names:
+            raise ValueError(
+                f"found key on {params.chosen.native}, should be on one of {curve_names}"
+            )
+
+        # Create PEM from key
+        curve = get_curve_for_oid(ObjectIdentifier(params.chosen.dotted))
+        public_pem = (
+            EllipticCurvePublicKey.from_encoded_point(
+                curve(), ECPoint().load(bytes(hsm_point)).native
+            )
+            .public_bytes(
+                serialization.Encoding.PEM,
+                serialization.PublicFormat.SubjectPublicKeyInfo,
+            )
+            .decode()
+        )
+
+        return SSlibKey(
+            sslib_keyid,
+            KEY_TYPE_ECDSA,
+            scheme_for_curve[curve],
+            {"public": public_pem},
+        )
+
     @classmethod
     def from_priv_key_uri(
         cls,
@@ -99,12 +205,7 @@ class HSMSigner(Signer):
         if secrets_handler is None:
             raise ValueError("HSMSigner requires a secrets handler")
 
-        # For now, we only support keyid 2, i.e. PIV slot 9c (Digital Signature)
-        # https://developers.yubico.com/PIV/Introduction/Certificate_slots.html
-        # https://developers.yubico.com/yubico-piv-tool/YKCS11/
-        hsm_keyid = 2
-
-        return cls(hsm_keyid, public_key, secrets_handler)
+        return cls(cls.PIV_9C_KEYID, public_key, secrets_handler)
 
     def sign(self, payload: bytes) -> Signature:
         """Signs payload with Hardware Security Module (HSM).
--- tests/test_hsm_signer.py
+++ tests/test_hsm_signer.py
@@ -8,22 +8,13 @@ import unittest
 
 from asn1crypto.keys import (  # pylint: disable=import-error
     ECDomainParameters,
-    ECPoint,
     NamedCurve,
 )
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric.ec import (
-    SECP256R1,
-    SECP384R1,
-    EllipticCurvePublicKey,
-    ObjectIdentifier,
-    get_curve_for_oid,
-)
+from cryptography.hazmat.primitives.asymmetric.ec import SECP256R1, SECP384R1
 from PyKCS11 import PyKCS11
 
 import securesystemslib.hash
-from securesystemslib import KEY_TYPE_ECDSA
-from securesystemslib.signer import HSMSigner, Signer, SSlibKey
+from securesystemslib.signer import HSMSigner, Signer
 from securesystemslib.signer._hsm_signer import PYKCS11LIB
 
 
@@ -124,85 +115,6 @@ class TestHSM(unittest.TestCase):
         shutil.rmtree(cls.test_dir)
         del os.environ["SOFTHSM2_CONF"]
 
-    @staticmethod
-    def _from_hsm(
-        hsm_session,
-        hsm_keyid,
-        keyid,
-    ):
-        """Export public key from HSM
-
-        Supports ecdsa on SECG curves secp256r1 (NIST P-256) or secp384r1 (NIST P-384).
-
-        Arguments:
-            hsm_session: An open ``PyKCS11.Session`` to the token with the public key.
-            hsm_keyid: Key identifier on the token.
-            keyid: Key identifier that is unique within the metadata it is used in.
-
-        Raises:
-            ValueError: No compatible key for ``hsm_keyid`` found on HSM.
-            PyKCS11.PyKCS11Error: Various HSM communication errors.
-
-        """
-        # if CRYPTO_IMPORT_ERROR:
-        #     raise exceptions.UnsupportedLibraryError(CRYPTO_IMPORT_ERROR)
-
-        # if PYKCS11_IMPORT_ERROR:
-        #     raise exceptions.UnsupportedLibraryError(PYKCS11_IMPORT_ERROR)
-
-        # Search for ecdsa public keys with passed keyid on HSM
-        keys = hsm_session.findObjects(
-            [
-                (PyKCS11.CKA_CLASS, PyKCS11.CKO_PUBLIC_KEY),
-                (PyKCS11.CKA_KEY_TYPE, PyKCS11.CKK_ECDSA),
-                (PyKCS11.CKA_ID, (hsm_keyid,)),
-            ]
-        )
-
-        # if len(keys) != 1:
-        #     raise ValueError(
-        #         f"hsm_keyid must identify one {KEY_TYPE_ECDSA} key, found {len(keys)}"
-        #     )
-
-        # Extract public key domain parameters and point from HSM
-        hsm_params, hsm_point = hsm_session.getAttributeValue(
-            keys[0], [PyKCS11.CKA_EC_PARAMS, PyKCS11.CKA_EC_POINT]
-        )
-
-        params = ECDomainParameters.load(bytes(hsm_params))
-
-        # TODO: Define as module level constant and don't hardcode scheme strings
-        scheme_for_curve = {
-            SECP256R1: "ecdsa-sha2-nistp256",
-            SECP384R1: "ecdsa-sha2-nistp384",
-        }
-        # curve_names = [curve.name for curve in scheme_for_curve]
-
-        # if params.chosen.native not in curve_names:
-        #     raise ValueError(
-        #         f"found key on {params.chosen.native}, should be on one of {curve_names}"
-        #     )
-
-        # Create PEM from key
-        curve = get_curve_for_oid(ObjectIdentifier(params.chosen.dotted))
-        public_pem = (
-            EllipticCurvePublicKey.from_encoded_point(
-                curve(), ECPoint().load(bytes(hsm_point)).native
-            )
-            .public_bytes(
-                serialization.Encoding.PEM,
-                serialization.PublicFormat.SubjectPublicKeyInfo,
-            )
-            .decode()
-        )
-
-        return SSlibKey(
-            keyid,
-            KEY_TYPE_ECDSA,
-            scheme_for_curve[curve],
-            {"public": public_pem},
-        )
-
     def test_hsm(self):
         """Test public key export, HSM signing, and verification w/o HSM"""
 
@@ -212,17 +124,13 @@ class TestHSM(unittest.TestCase):
             hasher.update(data)
             return hasher.digest()
 
-        lib = PYKCS11LIB()
-        hsm_slot_id = lib.getSlotList(tokenPresent=True)[0]
-        session = lib.openSession(hsm_slot_id)
-
         keyid = (
             "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
         )
         data = b"deadbeef"
 
         for hsm_keyid in [1, 2]:
-            public_key = self._from_hsm(session, hsm_keyid, keyid)
+            public_key = HSMSigner.pubkey_from_hsm(hsm_keyid, keyid)
 
             if hsm_keyid == 2:
                 signer = Signer.from_priv_key_uri(
@@ -242,8 +150,6 @@ class TestHSM(unittest.TestCase):
             sig = signer.sign(_pre_hash(data, public_key.scheme))
 
             public_key.verify_signature(sig, data)
-
-        session.closeSession()
 
 
 # Run the unit tests.
