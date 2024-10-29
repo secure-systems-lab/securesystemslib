1bf96df7b88eb1fc75a2272f5f911ad144058737
NicholasTanz <nicholastanzillo@gmail.com>
2024-10-29 00:54:12 -0400

    remove -dev-no-store-token option
    Signed-off-by: NicholasTanz <nicholastanzillo@gmail.com>

--- tests/scripts/init-vault.sh
+++ tests/scripts/init-vault.sh
@@ -3,7 +3,6 @@
 vault server -dev \
     -dev-listen-address="${VAULT_ADDR#http://}" \  # unlike VAULT_ADDR this option does not want the protocol prefix
     -dev-root-token-id="${VAULT_TOKEN}" \
-    -dev-no-store-token \
     &
 
 until vault status
