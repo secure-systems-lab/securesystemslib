e0fbb68dda2362bdef3c9506d5126871a366ff8a
Jussi Kukkonen <jkukkonen@google.com>
2025-02-03 12:45:32 +0200

    Stricter mypy setup
    * Move config to pyproject.toml
    * Turn on useful mypy options
    * Don't check tests with mypy: too much to fix right now
    * Add py.typed to announce this project is type annotated
    * Fix various annotation issues
    
    Issues remain in hash module
    
    Signed-off-by: Jussi Kukkonen <jkukkonen@google.com>

--- securesystemslib/signer/_utils.py
+++ securesystemslib/signer/_utils.py
@@ -9,7 +9,7 @@ from securesystemslib.formats import encode_canonical
 from securesystemslib.hash import digest
 
 
-def compute_default_keyid(keytype: str, scheme, keyval: dict[str, Any]) -> str:
+def compute_default_keyid(keytype: str, scheme: str, keyval: dict[str, Any]) -> str:
     """Return sha256 hexdigest of the canonical json of the key."""
     data: str | None = encode_canonical(
         {
